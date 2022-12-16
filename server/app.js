de86802a6cbc14c3a580393aa96b215f384c6ef5
Vladimir Antigua <vladimirantigua@gmail.com>
2022-12-16 10:39:46 -0800

    added the trail api

--- server/app.js
+++ server/app.js
@@ -6,12 +6,13 @@ const PORT = process.env.PORT; //port to listen make it PORT capitalize because 
 
 app.get("/", () => {
     console.log("App is getting to the Home Page");
+    // console.log(PORT)
 
 }); //create update delete destroy
 
 
 // API ENDPOINTS
-app.get("/api/weather", (req, res)=>{
+app.get("/api/weather", (req, res) => {
     const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
     const searchType = "current";
     const location = "London";
@@ -19,12 +20,61 @@ app.get("/api/weather", (req, res)=>{
         `http://api.weatherapi.com/v1/${searchType}.json?key=${WEATHER_API_KEY}&q=${location}`
     )
         .then((response) => response.json())
-        .then((data) => {console.log("Response: ", data);});
-        
+        .then((data) => { console.log("Response: ", data); });
+
+});
+
+//HIKE API:
+
+
+
+
+app.get('/api/test', () => {
+
+    const options = {
+
+        method: 'GET',
+
+        headers: {
+
+            'X-RapidAPI-Key': 'd4f3fb3b77mshb66b4c6e752d815p1fda49jsnf028cdccaa6f',
+
+            'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com'
+
+        }
+
+    };
+
+    fetch('https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=39.7551&lon=-75.5291', options)
+
+        .then(response => response.json())
+
+        .then(response => console.log(response))
+
+        .catch(err, console.error(err));
+
+});
+
+app.get('/api/trail', () => {
+    //Find Bike Trail API: 
+    const getAPIKey = process.env.TRAIL_API_KEY;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': getAPIKey,
+            'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=39.7551&lon=-75.5291', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
 });
 
 
-
+//Listening:
 app.listen(PORT, () => {
     console.log("Listening port: " + PORT);
 }); //  take two parameters app is listening on port 3000                                
